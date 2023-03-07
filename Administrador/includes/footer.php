354bd5345b1d295a13c377a5add0f78f312ae15a
MikeCz01 <miguelcruz5734@gmail.com>
2023-03-07 01:05:53 -0600

    Actualización

--- Administrador/includes/footer.php
+++ Administrador/includes/footer.php
@@ -5,6 +5,7 @@
     <script src="../js/main.js"></script>
     <!-- The javascript plugin to display page loading on top-->
     <script src="../js/plugins/pace.min.js"></script>
+    <script type="text/javascript" src="../js/plugins/sweetalert.min.js"></script>
     <!-- Page specific javascripts-->
     <!-- Google analytics script-->
     <!-- Data table plugin-->
--- Administrador/js/functions-usuarios.js
+++ Administrador/js/functions-usuarios.js
@@ -5,24 +5,7 @@ document.addEventListener('DOMContentLoaded', function(){
         "aProcessing": true,
         "aServerSide": true,
         "language": {
-            "sProcessing":     "Procesando...",
-            "sLengthMenu":     "Mostrar MENU registros",
-            "sZeroRecords":    "No se encontraron resultados",
-            "sEmptyTable":     "Ningún dato disponible en esta tabla",
-            "sInfo":           "Mostrando un total de TOTAL registros",
-            "sInfoEmpty":      "Mostrando un total de 0 registros",
-            "sInfoFiltered":   "(filtrado de un total de MAX registros)",
-            "sInfoPostFix":    "",
-            "sSearch":         "Buscar:",
-            "sUrl":            "",
-            "sInfoThousands":  ",",
-            "sLoadingRecords": "Cargando...",
-            "oPaginate": {
-                "sFirst":    "Primero",
-                "sLast":     "Último",
-                "sNext":     "Siguiente",
-                "sPrevious": "Anterior"
-            }
+            "url": "//cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json"
         },
         "ajax":{
             "url":"./models/usuarios/tablaUsuarios.php",
@@ -33,14 +16,48 @@ document.addEventListener('DOMContentLoaded', function(){
             {"data":"usuario_id"},
             {"data":"nombre"},
             {"data":"usuario"},
-            {"data":"rol"},
+            {"data":"nombre_rol"},
             {"data":"estado"}
         ],
         "responsive": true,
         "bDestroy":true,
         "iDisplayLength":10,
         "order": [[0,"asc"]]
-    })
+    });
+    var formUsuario = document.querySelector('#formUsuario');
+    formUsuario.onsubmit = function(e){
+        e.preventDefault();
+
+        var nombre = document.querySelector('#nombre').value;
+        var usuario = document.querySelector('#usuario').value;
+        var clave = document.querySelector('#clave').value;
+        var rol = document.querySelector('#listRol').value;
+        var estado = document.querySelector('#listEstado').value;
+
+        if(nombre == "" || usuario == "" || clave == ""){
+            swal('Atención','Todos los campos son obligatorios','warning');
+            return false;
+        }
+
+        var request = (window.XMLHttpRequest) ? new XMLHttpRequest : new ActiveXObject('Microsoft.XMLHTTP');
+        var url= './models/usuarios/ajax-usuarios.php';
+        var form = new FormData(formUsuario);
+        request.open('POST', url, true);
+        request.send(form);
+        request.onreadystatechange = function(){
+            if(request.readyState == 4 && request.status == 200){
+                var data = JSON.parse(request.responseText);
+                if(request.status == 200){
+                    $('#modalUsuario').modal('hide'); 
+                    formUsuario.reset();
+                    swal('Usuario',data.msg,'success');
+                    tableusuarios.ajax.reload();
+                }else{
+                    swal('Usuario',data.msg,'warning');
+                }
+            }
+        }
+    }
 })
 
 
