53ea9374d09639eedc10f4b62abccbab13f8d43b
walterdiaz1993 <waltermisael1108@gmail.com>
2023-03-07 18:36:57 -0600

    Se agrego login para alumnos

--- index.php
+++ index.php
@@ -25,6 +25,9 @@
                 <li class="nav-item" role="presentation">
                     <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Docente</button>
                 </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Alumno</button>
+                </li>
                 
                 </ul>
                 <div class="tab-content" id="myTabContent">
