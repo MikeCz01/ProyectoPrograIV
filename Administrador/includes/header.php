196bd7283fa575b67b568a9efb505305db72802b
MikeCz01 <miguelcruz5734@gmail.com>
2023-03-11 21:21:07 -0600

    CSS

--- Administrador/includes/header.php
+++ Administrador/includes/header.php
@@ -14,7 +14,7 @@
   </head>
   <body class="app sidebar-mini">
     <!-- Navbar-->
-    <header class="app-header"><a class="app-header__logo" href="index.html">Royal Legacy </a>
+    <header class="app-header"><a class="app-header__logo" href="index.php">Royal Legacy </a>
       <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
       <!-- Navbar Right Menu-->
       <ul class="app-nav">
