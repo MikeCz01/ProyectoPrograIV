72c52d497c9948f7a319c6612db7e91caa3a7cff
MikeCz01 <miguelcruz5734@gmail.com>
2023-03-12 18:15:52 -0600

    Boton Dismiss

--- Administrador/includes/modals/modal_actividades.php
+++ Administrador/includes/modals/modal_actividades.php
@@ -3,7 +3,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="tituloModalActividad">Nueva Actividad</h5>
-        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">X</button>
       </div>
       <div class="modal-body">
         <form id="formActividad" name="formActividad">
