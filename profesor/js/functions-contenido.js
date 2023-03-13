c6ea8c99051def16b4988562dc971efa623162f9
NelQuin9 <126970909+NelQuin9@users.noreply.github.com>
2023-03-12 18:19:52 -0600

    profesor

--- profesor/js/functions-contenido.js
+++ profesor/js/functions-contenido.js
@@ -34,13 +34,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     } else {
                         swal('Atencion',data.msg,'error');
                     }
-                }
-            }
-        }
-    }
-}
-
-)
+                })
 
 function openModalContenido(){
     document.querySelector('#idcontenido').value = "";
@@ -50,4 +44,36 @@ function openModalContenido(){
     $('#modalContenido').modal('show');
 
 }
+
+function editarContenido() {
+    var idcontenido = id;
+
+    document.querySelector('#tituloModal').innerHTML = 'Actualizar Contenido';
+    document.querySelector('#action').innerHTML = 'Actualizar';
+
+    var request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+        var url= './models/contenido/edit-contenido.php?idcontenido='+idcontenido;
+       
+        request.open('GET',url,true);
+        request.send();
+        request.onreadystatechange = function(){
+            if(request.readyState == 4 && request.status == 200) {
+                var data = JSON.parse(request.responseText);
+                if(data.status){
+                    document.querySelector('#idcontenido').value = data.data.contenido_id;
+                    document.querySelector('#titulo').value = data.data.titulo;
+                    document.querySelector('#descripcion').value = data.data.descripcion;
+
+                    $('#modalContenido').modal('show');
+                }  else {
+                    swal('Atencion', data.msg,'error');
+                }
+}
+        }
+    }
+}
+        }
+    }
+});
+
 
