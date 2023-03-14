f2bdeecfcad54fe75544731af00106474f696731
MikeCz01 <miguelcruz5734@gmail.com>
2023-03-14 03:13:38 -0600

    Calendario

--- Administrador/includes/footer.php
+++ Administrador/includes/footer.php
@@ -3,6 +3,9 @@
     <script src="../js/popper.min.js"></script>
     <script src="../js/bootstrap.min.js"></script>
     <script src="../js/main.js"></script>
+	<script type="text/javascript" src="../js/plugins/jquery-ui.custom.min.js"></script>
+	<script type="text/javascript" src="../js/plugins/moment.min.js"></script>
+	<script type="text/javascript" src="../js/plugins/fullcalendar.min.js"></script>
     <!-- The javascript plugin to display page loading on top-->
     <script src="../js/plugins/pace.min.js"></script>
     <script type="text/javascript" src="../js/plugins/sweetalert.min.js"></script>
@@ -19,5 +22,45 @@
     <script src="js/functions-materia.js"></script>
     <script src="js/functions-periodos.js"></script>
     <script src="js/functions-actividades.js"></script>
+	<script type="text/javascript">
+      $(document).ready(function() {
+      
+      	$('#external-events .fc-event').each(function() {
+      
+      		// store data so the calendar knows to render an event upon drop
+      		$(this).data('event', {
+      			title: $.trim($(this).text()), // use the element's text as the event title
+      			stick: true // maintain when user navigates (see docs on the renderEvent method)
+      		});
+      
+      		// make the event draggable using jQuery UI
+      		$(this).draggable({
+      			zIndex: 999,
+      			revert: true,      // will cause the event to go back to its
+      			revertDuration: 0  //  original position after the drag
+      		});
+      
+      	});
+      
+      	$('#calendar').fullCalendar({
+      		header: {
+      			left: 'prev,next today',
+      			center: 'title',
+      			right: 'month,agendaWeek,agendaDay'
+      		},
+      		editable: true,
+      		droppable: true, // this allows things to be dropped onto the calendar
+      		drop: function() {
+      			// is the "remove after drop" checkbox checked?
+      			if ($('#drop-remove').is(':checked')) {
+      				// if so, remove the element from the "Draggable Events" list
+      				$(this).remove();
+      			}
+      		}
+      	});
+      
+      
+      });
+    </script>
   </body>
 </html>
--- Administrador/index.php
+++ Administrador/index.php
@@ -4,18 +4,31 @@ require_once 'includes/header.php';
     <main class="app-content">
       <div class="app-title">
         <div>
-          <h1><i class="fa fa-dashboard"></i> Blank Page</h1>
-          <p>Start a beautiful journey here</p>
+          <h1><i class="fa fa-calendar"></i> Calendario</h1>
+          <p>Calendario de Actividades</p>
         </div>
         <ul class="app-breadcrumb breadcrumb">
           <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
-          <li class="breadcrumb-item"><a href="#">Blank Page</a></li>
+          <li class="breadcrumb-item"><a href="#">Calendario</a></li>
         </ul>
       </div>
       <div class="row">
         <div class="col-md-12">
-          <div class="tile">
-            <div class="tile-body">Create a beautiful dashboard</div>
+          <div class="tile row">
+            <div class="col-md-3">
+              <div id="external-events">
+                <h4 class="mb-4">Eventos Interactivos</h4>
+                <div class="fc-event">Evento</div>
+                <p class="animated-checkbox mt-20">
+                  <label>
+                    <input id="drop-remove" type="checkbox"><span class="label-text">Eliminar al soltar</span>
+                  </label>
+                </p>
+              </div>
+            </div>
+            <div class="col-md-9">
+              <div id="calendar"></div>
+            </div>
           </div>
         </div>
       </div>
