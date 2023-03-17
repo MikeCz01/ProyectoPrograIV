ecc796cfdd5dc87293fd1efc541089b62bfbba63
MikeCz01 <miguelcruz5734@gmail.com>
2023-03-16 21:47:32 -0600

    profesor-materia

---
+++ Administrador/includes/modals/modal_profesor_materia.php
@@ -0,0 +1,48 @@
+<div class="modal fade" id="modalProfesorMateria" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="tituloModalDocente">Nuevo Proceso</h5>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">X</button>
+      </div>
+      <div class="modal-body">
+        <form id="formProfesorMateria" name="formProfesorMateria">
+          <div class="form-group">
+            <label for="listEstado">Seleccione el Docente</label>
+            <select class="form-control"name="listProfesor" id="listProfesor">
+            <!--Contenido del ajax -->  
+            </select>
+          </div> 
+          <div class="form-group">
+            <label for="listEstado">Seleccione el Grado</label>
+            <select class="form-control"name="listGrado" id="listProfesor">
+            <!--Contenido del ajax -->  
+            </select>
+          </div> 
+          <div class="form-group">
+            <label for="listEstado">Seleccione el Aula</label>
+            <select class="form-control"name="listAula" id="listProfesor">
+            <!--Contenido del ajax -->  
+            </select>
+          </div> 
+          <div class="form-group">
+            <label for="listEstado">Seleccione la Materia</label>
+            <select class="form-control"name="listMateria" id="listProfesor">
+            <!--Contenido del ajax -->  
+            </select>
+          </div> 
+          <div class="form-group">
+            <label for="listEstado">Seleccione el Docente</label>
+            <select class="form-control"name="listProfesor" id="listProfesor">
+            <!--Contenido del ajax -->  
+            </select>
+          </div> 
+            <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+            <button class="btn btn-primary" id="actionProfesorMateria" type="submit">Guardar</button>
+        </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
